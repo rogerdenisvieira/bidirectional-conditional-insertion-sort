5fa7623327b4cc6be87263087d6cbb8f8689a5fc
Roger Vieira <roger_v@dell.com>
2019-11-01 19:13:28 -0300

    coding measurement engine

--- main.py
+++ main.py
@@ -1,18 +1,83 @@
-from sorters import BCIS
+from sorters import BCIS, QS
 from random import randrange
+from timeit import default_timer as timer
+import numpy as np
+import sys, os, csv, io
 
 
-def seed_random(size, min, max):
+#############################################################
+#                                                           #
+#              Data Preparation and Setup                   #
+#                                                           #
+#############################################################
 
-    array = []
-    for i in range(0, size):
-        array.append(randrange(min, max))
-    return array
-
+binominal_dist = np.random.binomial(1000, .5, 10000)
+poisson_dist = np.random.poisson(1000, 10000)
+normal_dist = np.random.normal(0, .1, 10000)
+uniform_dist1 = np.random.uniform(-1,1,10000)
 
 bcis_sorter = BCIS()
-array = seed_random(100, 0, 500)
+qs_sorter = QS()
 
-bcis_sorter.sort(array, 0, len(array)-1)
+total_ratios = []
+
+
+
+# a workaround to avoid Python's recursion limit
+sys.setrecursionlimit(6100)
+
+
+def write_report(values):
+    # output file with a list of ratios
+    output_file = open('ratios.csv', 'w')
+
+    try:
+        #csv_writer = file.writer(output_file)
+
+        for value in values:
+            output_file.write(str(value) + "\n")
+    finally:
+        output_file.close()
+
+
+def get_ratio_bcis_qs(distribution):
+    # measuring BCIS execution elapsed time
+    start = timer()
+    bcis_sorter.sort(distribution, 0, len(distribution)-1)
+    end = timer()
+
+    elapsed_time_bcis = end - start
+
+    # measuring QS execution elapsed time
+    start = timer()
+    qs_sorter.sort(distribution, 0, len(distribution)-1)
+    end = timer()
+
+    elapsed_time_qs = end - start
+    ratio = elapsed_time_bcis/elapsed_time_qs
+
+    return "{0};{1};{2};".format(
+        elapsed_time_bcis, 
+        elapsed_time_qs, 
+        ratio)
+
+#############################################################
+#                                                           #
+#                  Measurement Execution                    #
+#                                                           #
+#############################################################
+
+# executing comparisons throught N trials
+for i in range(0, 6000, 100):
+    uniform_dist = np.random.uniform(-1,1,i)
+
+    partial_ratio = get_ratio_bcis_qs(uniform_dist)  
+    total_ratios.append(partial_ratio)
+
+    if i%10 == 0:
+        print("Array size: {0} Ratio BCIS/QS: {1}".format(i, partial_ratio))
+
+
+write_report(total_ratios)
 
 
--- sorters.py
+++ sorters.py
@@ -3,21 +3,19 @@ import math
 
 class BCIS:
 
-    def print_array(self, array):
+    def __print_array(self, array):
         for i in array:
             print("{0}".format(i), end =" ")        
         print("")
 
     
-    def swap(self, array, i, j):
-        temp = array[i]
-        array[i] = array[j]
-        array[j] = temp
+    def __swap(self, array, first_index, second_index):
+        temp = array[first_index]
+        array[first_index] = array[second_index]
+        array[second_index] = temp
 
-        # printing arrays's content
-        self.print_array(array)
 
-    def insert_right(self, array, current_item, sr, right):
+    def __insert_right(self, array, current_item, sr, right):
         j = sr
         while(j <= right and current_item > array[j]):
             array[j - 1] = array[j]
@@ -26,7 +24,7 @@ class BCIS:
         array[j - 1] = current_item
 
 
-    def insert_left(self, array, current_item, sl, left):
+    def __insert_left(self, array, current_item, sl, left):
         j = sl
         while(j >= left and current_item < array[j]):
             array[j+1] = array[j]
@@ -35,38 +33,37 @@ class BCIS:
         array[j + 1] = current_item
 
 
-    def is_equals(self, array, sl, sr):
-        sl_more = sl + 1
-        sr_less = sr - 1
+    def __is_equals(self, array, sl_index, sr_index):
+        sl_more = sl_index + 1
+        sr_less = sr_index - 1
 
         for k in range(sl_more, sr_less):
-            if(array[k] != array[sl]):
-                self.swap(array, k, sl)
+            if(array[k] != array[sl_index]):
+                self.__swap(array, k, sl_index)
                 return k
         return -1
 
-    def sort(self, array, left, right):
-        sl = left
-        sr = right
+    def sort(self, array, sl_index, sr_index):
+        sl = sl_index
+        sr = sr_index
 
-        while(sl < sr and sr <= right):
+        while(sl < sr and sr <= sr_index):
             calc_1= sl + ((sr - sl)//2)
-            self.swap(array, sr, calc_1)
+            self.__swap(array, sr, calc_1)
 
             if(array[sl] == array[sr]):
-                if(self.is_equals(array, sl, sr) != -1):
+                if(self.__is_equals(array, sl, sr) != -1):
                     return -1
             
             if(array[sl] > array[sr]):
-                self.swap(array, sl, sr)
+                self.__swap(array, sl, sr)
 
             if(sl - sr >= 100):
-                rnge = range((sl + 1), math.sqrt(sr - sl))
                 for i in range((sl + 1), math.sqrt(sr - sl)):
                     if(array[sr] < array[i]):
-                        self.swap(array, sr, i)
+                        self.__swap(array, sr, i)
                     elif(array[sl] > array[i]):
-                        self.swap(array, sl, i)
+                        self.__swap(array, sl, i)
             else:
                 i = sl + 1
 
@@ -78,12 +75,12 @@ class BCIS:
 
                 if(current_item >= rc):
                     array[i] = array[sr - 1]
-                    self.insert_right(array, current_item, sr, right)
+                    self.__insert_right(array, current_item, sr, sr_index)
                     sr = sr - 1
 
                 elif(current_item <= lc):
                     array[i] = array[sl + 1]
-                    self.insert_left(array, current_item, sl, left)
+                    self.__insert_left(array, current_item, sl, sl_index)
                     sl += 1
                     i += 1
                 else:
@@ -91,6 +88,50 @@ class BCIS:
             sl += 1
             sr += 1
         return 1
+
+class QS:
+
+    # Python program for implementation of Quicksort Sort 
+
+    # This function takes last element as pivot, places 
+    # the pivot element at its correct position in sorted 
+    # array, and places all smaller (smaller than pivot) 
+    # to left of pivot and all greater elements to right 
+    # of pivot 
+    def __partition(self,arr,low,high): 
+        i = ( low-1 )		 # index of smaller element 
+        pivot = arr[high]	 # pivot 
+
+        for j in range(low , high): 
+
+            # If current element is smaller than or 
+            # equal to pivot 
+            if arr[j] <= pivot: 
+            
+                # increment index of smaller element 
+                i = i+1
+                arr[i],arr[j] = arr[j],arr[i] 
+
+        arr[i+1],arr[high] = arr[high],arr[i+1] 
+        return ( i+1 ) 
+
+    # The main function that implements QuickSort 
+    # arr[] --> Array to be sorted, 
+    # low --> Starting index, 
+    # high --> Ending index 
+
+    # Function to do Quick sort 
+    def sort(self,arr,low,high): 
+        if low < high: 
+
+            # pi is partitioning index, arr[p] is now 
+            # at right place 
+            pi = self.__partition(arr,low,high) 
+
+            # Separately sort elements before 
+            # partition and after partition 
+            self.sort(arr, low, pi-1) 
+            self.sort(arr, pi+1, high) 
 
 
 
