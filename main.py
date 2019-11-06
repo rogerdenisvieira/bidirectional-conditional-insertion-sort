b68d022e7083ee4e3ee50eb129c385a5b49aa55e
Roger Vieira <rogerdenis@gmail.com>
2019-11-06 06:35:12 -0300

    WIP: replicated measurement logic for all others distributions

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from sorters import BCIS, QS
-from random import randrange
+from sorters import BCIS, QS, IS
 from timeit import default_timer as timer
 import numpy as np
+import pandas as pd
 import sys, os, csv, io
 
 
@@ -11,15 +11,18 @@ import sys, os, csv, io
 #                                                           #
 #############################################################
 
-binominal_dist = np.random.binomial(1000, .5, 10000)
-poisson_dist = np.random.poisson(1000, 10000)
-normal_dist = np.random.normal(0, .1, 10000)
-uniform_dist1 = np.random.uniform(-1,1,10000)
+TEST_SIZE = 100 # max distribution size
+
+uniform_dist = np.random.uniform(-1,1,TEST_SIZE),
+binominal_dist = np.random.binomial(1000, .5, TEST_SIZE),
+poisson_dist = np.random.poisson(1000, TEST_SIZE),
+normal_dist = np.random.normal(0, .1, TEST_SIZE)
 
 bcis_sorter = BCIS()
 qs_sorter = QS()
+is_sorter = IS()
 
-total_ratios = []
+final_results = []
 
 
 
@@ -28,19 +31,24 @@ sys.setrecursionlimit(6100)
 
 
 def write_report(values):
-    # output file with a list of ratios
-    output_file = open('ratios.csv', 'w')
+    filename = 'ratios.csv'
+    fieldnames = ['name', 'size', 'bcis', 'qs', 'is','bcis/qs','bcis/is']
+    print('Writings report...')
 
     try:
-        #csv_writer = file.writer(output_file)
+        output_file = open(filename, 'w', newline='')
+        csv_writer = csv.DictWriter(output_file, fieldnames=fieldnames)
+        csv_writer.writeheader()
 
-        for value in values:
-            output_file.write(str(value) + "\n")
+        for row in values:
+            csv_writer.writerow(row)
+            
     finally:
         output_file.close()
 
+    
+def calc_execution_elapsed_times(distribution):  
 
-def get_ratio_bcis_qs(distribution):
     # measuring BCIS execution elapsed time
     start = timer()
     bcis_sorter.sort(distribution, 0, len(distribution)-1)
@@ -54,12 +62,21 @@ def get_ratio_bcis_qs(distribution):
     end = timer()
 
     elapsed_time_qs = end - start
-    ratio = elapsed_time_bcis/elapsed_time_qs
 
-    return "{0};{1};{2};".format(
-        elapsed_time_bcis, 
-        elapsed_time_qs, 
-        ratio)
+    # measuring IS execution elapsed time
+    start = timer()
+    is_sorter.sort(distribution)
+    end = timer()
+
+    elapsed_time_is = end - start
+
+    return {
+        'bcis' : elapsed_time_bcis,
+        'qs' : elapsed_time_qs,
+        'is' : elapsed_time_is
+    }
+
+
 
 #############################################################
 #                                                           #
@@ -68,16 +85,40 @@ def get_ratio_bcis_qs(distribution):
 #############################################################
 
 # executing comparisons throught N trials
-for i in range(0, 6000, 100):
-    uniform_dist = np.random.uniform(-1,1,i)
+for i in range(0, TEST_SIZE, 1):
 
-    partial_ratio = get_ratio_bcis_qs(uniform_dist)  
-    total_ratios.append(partial_ratio)
+    #distributions = {
+    #    'uniform_dist' : np.random.uniform(-1,1,i),
+    #    'binominal_dist' : np.random.binomial(1000, .5, i),
+    #    'poisson_dist' : np.random.poisson(1000, i),
+    #    'normal_dist' : np.random.normal(0, .1, i)
+    #}
 
+
+
+    uniform_dist_partial_results = calc_execution_elapsed_times(uniform_dist[0:i])
+    binominal_dist_partial_results = calc_execution_elapsed_times(uniform_dist[0:i])
+    poisson_dist_partial_results = calc_execution_elapsed_times(uniform_dist[0:i])
+    normal_dist_partial_results = calc_execution_elapsed_times(uniform_dist[0:i])
+
+    print("Size: {0}".format(len(uniform_dist[0:i])))
+
+
+    #print("{0} # {1} # {2} # {3}".format(
+    #    uniform_dist_partial_results,
+    #    binominal_dist_partial_results,
+    #    poisson_dist_partial_results,
+    #    normal_dist_partial_results
+    #))
+
+
+
+    
     if i%10 == 0:
-        print("Array size: {0} Ratio BCIS/QS: {1}".format(i, partial_ratio))
+        print("Array size: {0}".format(i))
 
 
-write_report(total_ratios)
+#print(total_ratios)
+write_report(final_results)
 
 
--- sorters.py
+++ sorters.py
@@ -121,18 +121,36 @@ class QS:
     # high --> Ending index 
 
     # Function to do Quick sort 
-    def sort(self,arr,low,high): 
+    def sort(self,array,low,high): 
         if low < high: 
 
             # pi is partitioning index, arr[p] is now 
             # at right place 
-            pi = self.__partition(arr,low,high) 
+            pi = self.__partition(array,low,high) 
 
             # Separately sort elements before 
             # partition and after partition 
-            self.sort(arr, low, pi-1) 
-            self.sort(arr, pi+1, high) 
+            self.sort(array, low, pi-1) 
+            self.sort(array, pi+1, high) 
 
+class IS:
+
+    def sort(self, array): 
+  
+        # Traverse through 1 to len(arr) 
+        for i in range(1, len(array)): 
+    
+            key = array[i] 
+    
+            # Move elements of arr[0..i-1], that are 
+            # greater than key, to one position ahead 
+            # of their current position 
+            j = i-1
+            while j >=0 and key < array[j] : 
+                    array[j+1] = array[j] 
+                    j -= 1
+            array[j+1] = key 
+    
 
 
 
