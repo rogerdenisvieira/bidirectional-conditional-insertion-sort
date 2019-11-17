309fd4e2c505718fdcb876b0a62ce457a595d2b3
Roger Vieira <rogerdenis@gmail.com>
2019-11-17 15:05:39 -0300

    Finished performance tests

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ import sys, os, csv, io
 #############################################################
 
 TEST_SIZE = 7000 # max distribution size = 10k
-STEP = 1
+STEP = 10
 REPORT_FILENAME = 'ratios.csv'
 DISTRIBUTIONS_FILENAME = 'distributions.csv'
 
@@ -31,7 +31,7 @@ normal_dist = distributions['normal']
 real_dist = distributions['real']
 
 bcis_sorter = BCIS()
-qs_sorter = QS()
+#qs_sorter = QS()
 is_sorter = IS()
 
 final_results = []
@@ -57,21 +57,24 @@ def write_report(values):
         output_file.close()
 
     
-def calc_execution_elapsed_times(distribution):  
+def calc_execution_elapsed_times(distribution, dist_name):  
 
     # measuring BCIS execution elapsed time
+
     start = timer()
     bcis_sorter.sort(distribution, 0, len(distribution)-1)
     end = timer()
 
     elapsed_time_bcis = end - start
+    print("BCIS for size: {0} took {1} for {2}".format(len(distribution), elapsed_time_bcis, dist_name))
 
     # measuring QS execution elapsed time
-    start = timer()
-    qs_sorter.sort(distribution, 0, len(distribution)-1)
-    end = timer()
-
-    elapsed_time_qs = end - start
+    #start = timer()
+    #qs_sorter.sort(distribution, 0, len(distribution)-1)
+    #end = timer()
+#
+    #elapsed_time_qs = end - start
+    #print("QS for size: {0} took {1} for {2}".format(len(distribution), elapsed_time_qs, dist_name))
 
     # measuring IS execution elapsed time
     start = timer()
@@ -79,12 +82,13 @@ def calc_execution_elapsed_times(distribution):
     end = timer()
 
     elapsed_time_is = end - start
+    print("IS for size: {0} took {1} for {2}".format(len(distribution), elapsed_time_is, dist_name))
 
     return {
         'bcis' : elapsed_time_bcis,
-        'qs' : elapsed_time_qs,
+        #'qs' : elapsed_time_qs,
         'is' : elapsed_time_is,
-        'bcis/qs' : (elapsed_time_bcis/elapsed_time_qs),
+        #'bcis/qs' : (elapsed_time_bcis/elapsed_time_qs),
         'bcis/is' : (elapsed_time_bcis/elapsed_time_is)
     }
 
@@ -101,52 +105,43 @@ try:
     # executing comparisons throught N trials
     for i in range(STEP, TEST_SIZE, STEP):
 
-        uniform_res = calc_execution_elapsed_times(uniform_dist[0:i])
-        binominal_res = calc_execution_elapsed_times(binominal_dist[0:i])
-        poisson_res = calc_execution_elapsed_times(poisson_dist[0:i])
-        normal_res = calc_execution_elapsed_times(normal_dist[0:i])
-        real_res = calc_execution_elapsed_times(real_dist[0:i])
+        print("Array size: {0}".format(i))
+
+        uniform_res = calc_execution_elapsed_times(uniform_dist[0:i], "uniform")
+        binominal_res = calc_execution_elapsed_times(binominal_dist[0:i], "binominal")
+        poisson_res = calc_execution_elapsed_times(poisson_dist[0:i], "poisson")
+        normal_res = calc_execution_elapsed_times(normal_dist[0:i], "normal")
+        real_res = calc_execution_elapsed_times(real_dist[0:i], "real")
+
 
 
         partial_result = {
             'array_size' : i,
 
             'uniform_bcis' : uniform_res['bcis'],
-            'uniform_qs' : uniform_res['qs'],
             'uniform_is' : uniform_res['is'],
-            'uniform_bcis/qs' : uniform_res['bcis/qs'],
             'uniform_bcis/is' : uniform_res['bcis/is'],
 
             'binomial_bcis' : binominal_res['bcis'],
-            'binomial_qs' : binominal_res['qs'],
             'binomial_is' : binominal_res['is'],
-            'binomial_bcis/qs' : binominal_res['bcis/qs'],
             'binomial_bcis/is' : binominal_res['bcis/is'],
 
             'poisson_bcis' : poisson_res['bcis'],
-            'poisson_qs' : poisson_res['qs'],
             'poisson_is' : poisson_res['is'],
-            'poisson_bcis/qs' : poisson_res['bcis/qs'],
             'poisson_bcis/is' : poisson_res['bcis/is'],
 
             'normal_bcis' : normal_res['bcis'],
-            'normal_qs' : normal_res['qs'],
             'normal_is' : normal_res['is'],
-            'normal_bcis/qs' : normal_res['bcis/qs'],
             'normal_bcis/is' : normal_res['bcis/is'],
 
             'real_bcis' : real_res['bcis'],
-            'real_qs' : real_res['qs'],
             'real_is' : real_res['is'],
-            'real_bcis/qs' : real_res['bcis/qs'],
             'real_bcis/is' : real_res['bcis/is']
         }
 
 
         final_results.append(partial_result)
-        
-        if i%10 == 0:
-            print("Array size: {0}".format(i))
+
 
 except:
     print("An error has been occurred")
